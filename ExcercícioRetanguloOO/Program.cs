aea63157710bc82c35a3d62b310cfac68e55fd95
Felipe Schein <felipe_str@hotmail.com>
2020-06-08 14:20:28 -0300

    projeto de cadastro Retangulos

---
+++ ExcercícioRetanguloOO/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExcercícioRetanguloOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Retangulo ret = new Retangulo();
+
+            Console.WriteLine("Entre a largura e a altura do retângulo: ");
+            ret.Largura = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            ret.Altura = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+            Console.WriteLine("AREA = "+ ret.Area().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("PERÍMETRO = " + ret.Perimetro().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("DIAGONAL = " + ret.Diagonal().ToString("F2", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
---
+++ ExcercícioRetanguloOO/Retangulo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExcercícioRetanguloOO
+{
+    class Retangulo
+    {
+        public double Largura;
+        public double Altura;
+        
+
+        public double Area()
+        {
+            return Largura * Altura;
+        }
+
+        public double Perimetro()
+        {
+            return 2 * Largura + 2 * Altura;
+        }
+
+        public double Diagonal()
+        {
+            return Math.Sqrt(Largura * Largura + Altura * Altura);
+        }
+    
+    
+    
+    
+    }
+
+
+}
